42413e529c4904522046ce97979a31038caa6941
moyuanchen <chenmoyuan1@gmail.com>
2025-04-25 20:10:08 +0100

    s1

--- simulate.py
+++ simulate.py
@@ -2,11 +2,11 @@ from util import simulate
 from config import Config
 
 
-c = Config(sigma_u=1)
+c = Config(sigma_u=10)
 counter = 0
 convergence_threshold = 1000000
 while counter < convergence_threshold:
-    log, agents = simulate(T = 500000, config = c, save_path='~/collusion/data/sigma_u_1e0.pkl')
+    log, agents = simulate(T = 500000, config = c, save_path='~/collusion/data/sigma_u_10.pkl')
     # Check if the agents have converged
     counter = min([agent.convergence_counter for agent in agents])
 
