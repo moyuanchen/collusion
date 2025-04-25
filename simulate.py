486da226b96f8e91ea69806d48ad4356d7b611ad
moyuanchen <chenmoyuan1@gmail.com>
2025-04-25 19:50:34 +0100

    full_simulation

--- simulate.py
+++ simulate.py
@@ -3,6 +3,10 @@ from config import Config
 
 
 c = Config()
-
-simulate(T = 1000, config = c, save_path='./simulated_data.pkl')
+counter = 0
+convergence_threshold = 1000000
+while counter < convergence_threshold:
+    log, agents = simulate(T = 500000, config = c, save_path='~/collusion/data/sigma_u_1e-1.pkl')
+    # Check if the agents have converged
+    counter = min([agent.convergence_counter for agent in agents])
 
