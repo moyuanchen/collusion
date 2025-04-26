9583774258527aa67a9acb977f61f6e0a74f15a0
moyuanchen <chenmoyuan1@gmail.com>
2025-04-26 12:10:09 +0100

    folder path

--- simulate.py
+++ simulate.py
@@ -6,7 +6,8 @@ c = Config(sigma_u=10)
 counter = 0
 convergence_threshold = 1000000
 while counter < convergence_threshold:
-    log, agents = simulate(T = 50, config = c, save_path='./data/sigma_u_10')
+    log, agents = simulate(T = 50, config = c, 
+                           save_path='/rds/general/user/mc4724/home/data/sigma_u_10')
     # Check if the agents have converged
     counter = min([agent.convergence_counter for agent in agents['informed']])
     break
