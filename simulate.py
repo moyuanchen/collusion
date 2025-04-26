6df9a0612eef30beda5a8c3f8825c326a162158f
moyuanchen <chenmoyuan1@gmail.com>
2025-04-26 12:00:11 +0100

    job folder

--- simulate.py
+++ simulate.py
@@ -6,6 +6,7 @@ c = Config(sigma_u=10)
 counter = 0
 convergence_threshold = 1000000
 while counter < convergence_threshold:
-    log, agents = simulate(T = 500000, config = c, save_path='./sigma_u_10')
+    log, agents = simulate(T = 50, config = c, save_path='./sigma_u_10')
     # Check if the agents have converged
     counter = min([agent.convergence_counter for agent in agents['informed']])
+    break
