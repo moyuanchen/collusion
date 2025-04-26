a9b3b5eaba9c380ef8036c402f3c1f0f41abaac2
moyuanchen <chenmoyuan1@gmail.com>
2025-04-26 13:30:45 +0100

    variable sigmau

--- simulate.py
+++ simulate.py
@@ -1,12 +1,25 @@
 from util import simulate
 from config import Config
+import argparse, json, numpy as np
+from pathlib import Path
 
+# parse command line arguments
+parser = argparse.ArgumentParser(
+    description="Run one simulation with an optional sigma_u override."
+)
+parser.add_argument("--sigma_u", type=float,
+                    help="Override noise-trader σᵤ (float)")
+args = parser.parse_args()
 
 c = Config(sigma_u=100)
+
+if args.sigma_u is not None:
+    c.sigma_u = args.sigma_u
+
 counter = 0
 convergence_threshold = 1000000
 while counter < convergence_threshold:
-    log, agents = simulate(T = 50, config = c, 
-                           save_path='/rds/general/user/mc4724/home/data/sigma_u_100')
+    log, agents = simulate(T = 500000, config = c, 
+                           save_path='/rds/general/user/mc4724/home/data/sigma_u_' + str(c.sigma_u))
     # Check if the agents have converged
     counter = min([agent.convergence_counter for agent in agents['informed']])
