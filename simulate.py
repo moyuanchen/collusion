c8655fee1e6799a64ea2834fae22ce55495bd618
moyuanchen <chenmoyuan1@gmail.com>
2025-04-27 10:03:37 +0100

    test2

--- simulate.py
+++ simulate.py
@@ -15,11 +15,13 @@ c = Config(sigma_u=100)
 
 if args.sigma_u is not None:
     c.sigma_u = args.sigma_u
-
+print(f"Running simulation with σᵤ = {c.sigma_u}")
 counter = 0
 convergence_threshold = 1000000
 while counter < convergence_threshold:
-    log, agents = simulate(T = 500000, config = c, 
+    log, agents = simulate(T = 50, config = c, 
                            save_path='/rds/general/user/mc4724/home/data/sigma_u_' + str(c.sigma_u))
     # Check if the agents have converged
     counter = min([agent.convergence_counter for agent in agents['informed']])
+    print(f"Convergence counter: {counter}")
+    break
