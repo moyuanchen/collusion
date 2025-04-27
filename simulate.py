ca5101c9f8065b9e032e47c712005d72e3ba19aa
moyuanchen <chenmoyuan1@gmail.com>
2025-04-27 10:05:56 +0100

    longer loop

--- simulate.py
+++ simulate.py
@@ -19,7 +19,7 @@ print(f"Running simulation with σᵤ = {c.sigma_u}")
 counter = 0
 convergence_threshold = 1000000
 while counter < convergence_threshold:
-    log, agents = simulate(T = 50, config = c, 
+    log, agents = simulate(T = 500000, config = c, 
                            save_path='/rds/general/user/mc4724/home/data/sigma_u_' + str(c.sigma_u))
     # Check if the agents have converged
     counter = min([agent.convergence_counter for agent in agents['informed']])
